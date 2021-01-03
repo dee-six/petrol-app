7bce0bb37e019d369355a7a1a1cbe55bfb90531d
dee-six <deepak.rampoore@gmail.com>
2021-01-03 22:49:22 +0100

    [Improve] Module petrol-microservice-app renamed to petrol-app

---
+++ petrol-app/src/main/java/ch/diyamane/app/petrol/PetrolApplication.java
@@ -0,0 +1,17 @@
+package ch.diyamane.app.petrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Application
+ *
+ */
+@SpringBootApplication
+public class PetrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetrolApplication.class, args);
+	}
+
+}
